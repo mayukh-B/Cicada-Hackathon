7a9e9e6cd148b074736fb458c677b7f775d01b3a
mayukh-B <rudrabagchi11@gmail.com>
2021-05-28 21:09:11 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+// const passport = require("passport");
+// const session = require("express-session");
+// const passportLocalMongoose = require("passport-local-mongoose");
+
+const app = express()
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
+
+mongoose.connect(
+  `mongodb+srv://${process.env.ADMIN}:${process.env.PASSWORD}@cluster0.cebu5.mongodb.net/cicadaDB?retryWrites=true&w=majority`,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+)
+
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
+app.listen(5000, () => {
+  console.log('server running at port 5000')
+})
